695fe8bf0d61b416f1d83dfffed477a3f162cce3
ngocha0303 <48287488+ngocha0303@users.noreply.github.com>
2019-04-16 14:54:25 +0700

    AccountModel

---
+++ ManagerCoffeeShopASPNet/Areas/Admin/Models/AccountModel.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagerCoffeeShopASPNet.Areas.Admin.Models
+{
+    [JsonObject]
+    public class AccountModel
+    {
+        [JsonProperty(PropertyName = "EmployeeID")]
+        public int EmployeeID { get; set; }
+        [JsonProperty(PropertyName = "UserName")]
+        public string UserName { get; set; }
+        [JsonProperty(PropertyName = "Password")]
+        public string Password { get; set; }
+        [JsonProperty(PropertyName = "AccType")]
+        public string AccType { get; set; }
+        [JsonProperty(PropertyName = "Position")]
+        public string Position { get; set; }
+        [JsonProperty(PropertyName = "Avatar")]
+        public string Avatar { get; set; }
+        public HttpPostedFileBase AvatarUpload { get; set; }
+    }
+}
